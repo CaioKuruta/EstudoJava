7e1f628d83222000fd09e4f7d9cae13d7830e354
ckuruta <caio.augusto0909@gmail.com>
2024-08-17 17:14:19 -0300

    Modificando aula 01

--- Aula01/ClassePrincipal.java
+++ Aula01/ClassePrincipal.java
@@ -1,10 +1,29 @@
 public class ClassePrincipal {
 
         public static void main(String[] args) {
+
+            int numero = 07;
             String primeiroNome = "Caio";
             String segundoNome = "Oliveira";
             System.out.printf(nomeCompleto(primeiroNome,segundoNome));
+            System.out.println(numero);
+
+            int n1 = 8;
+            int n2 = 15;
+            String resultado;
+
+            if (n1 > n2)
+                {resultado = "true";}
+            else
+                {resultado = "false";}
+
+            System.out.println("Resultado com if e else = "+resultado);
+
+            resultado = n1 == n2 ? "verdadeiro" : "falso";
+
+            System.out.println("Resultado com operador ternario = "+resultado);
         }
+
 
         public static String nomeCompleto(String primeiroNome,String segundoNome){
             return "Primeiro teste java : " + primeiroNome.concat(" ").concat(segundoNome);
