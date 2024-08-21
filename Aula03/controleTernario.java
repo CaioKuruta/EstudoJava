f5976131ff0f22e50b6becafdbe4fcee997c4df5
ckuruta <caio.augusto0909@gmail.com>
2024-08-21 14:39:55 -0300

    Atualização 2108

--- Aula03/controleTernario.java
+++ Aula03/controleTernario.java
@@ -2,10 +2,8 @@ public class controleTernario {
     public static void main(String[] args) {
         int nota = 6;
         String quero = "nao";
-        String resultado = nota >= 7 ? "Aprovado" : nota>=5 && nota<=7 ? "Recuperação":"Reprovado";
+        String resultado = nota >= 6 ? "Aprovado" : nota>=5 && nota<=7 ? "Recuperação":"Reprovado";
         System.out.println(resultado);
-
-
         String SeQuiserSim = quero == "sim" ? "SIM" : "NAO";
         System.out.println("\nSe quiser "+SeQuiserSim);
     }
