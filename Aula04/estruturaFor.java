3ea584d54225b68b2390e577498a8e85033fc35e
ckuruta <caio.augusto0909@gmail.com>
2024-08-22 11:52:59 -0300

    Subindo aula de repetições

--- Aula04/estruturaFor.java
+++ Aula04/estruturaFor.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class estruturaFor {
+    public static void main(String[] args) {
+        for (int carneiro = 1;carneiro < 20;carneiro++){
+            System.out.println("Contagem de carneiro -> "+carneiro);
+        }
+    }
 }
+
