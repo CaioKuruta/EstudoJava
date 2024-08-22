c4e5e348aeb4cb3c9cb011f950499fd5a2bf7dae
ckuruta <caio.augusto0909@gmail.com>
2024-08-22 12:16:18 -0300

    Finalizando estrutura de repetição

--- Aula04/estruturaArray.java
+++ Aula04/estruturaArray.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class estruturaArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Alex","Jonas","Carlos","Julha"};
+
+        for (int a = 0;a < alunos.length;a++){
+            System.out.println(alunos[a]);
+            System.out.println("Volta : "+a);
+        }
+
+        for(String aluno : alunos){
+            System.out.println("*Nome do aluno é "+aluno);
+        }
+    }
 }
